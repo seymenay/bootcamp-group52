8b284e432b544a70016bd65854f0c058cf347217
sena <eminesenatop03@gmail.com>
2024-07-30 01:06:48 +0300

    firebase updated

--- sinefili/android/app/src/main/kotlin/com/example/sinefili/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sinefili
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ sinefili_app/android/app/src/main/kotlin/com/example/sinefili_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sinefili_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
